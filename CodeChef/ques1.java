4533fa77186fd188c0e26a5cc64dfbd87e068437
nirvik34 <jarhnirvik@gmail.com>
2026-02-25 22:45:52 +0530

    feat(core): add ques1 and 1356

---
+++ CodeChef/ques1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a = sc.nextInt();
+
+        if (n * a >= m) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
